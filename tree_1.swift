4b4ffc086b4b424ce66d372c533f728b0439cb90
rezaulislam <mrezaulislam.t@gmail.com>
2025-05-31 21:00:24 +0600

    insertValue function added to insert value in tree

--- tree_1.swift
+++ tree_1.swift
@@ -6,3 +6,51 @@ final class TreeNode {
         self.value = value
     }
 }
+
+func insetValue(_ root : TreeNode?, _ value: Int) -> TreeNode {
+    guard let root = root else {
+        return TreeNode(value: value)
+    }
+    var current = root
+
+    while true {
+        if value < current.value {
+            if let tmp = current.left {
+                current = tmp
+            }else{
+                current.left = TreeNode(value: value)
+                return current.left!
+            }
+             
+        }else {
+            if let tmp = current.right{
+                current = tmp
+            }else{
+                current.right = TreeNode(value: value)
+                return current.right!
+            }
+        }
+    }
+  
+    return current
+}
+
+let rootTree = TreeNode(value: 10)
+var res = insetValue(rootTree, 5)
+res = insetValue(rootTree, 15)
+res = insetValue(rootTree, 12)
+res = insetValue(rootTree, 16)
+print("Result \(res.value)")
+
+
+//10, 5, 15, 12
+/*
+if value < current.value {
+        current.left = TreeNode(value : value)
+        current = current.left!
+        
+    }else{
+        current.right = TreeNode(value: value)
+        current = current.right!
+    }
+*/
