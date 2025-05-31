fcbc2264247fed7575410e71128fc93f4af3235e
rezaulislam <mrezaulislam.t@gmail.com>
2025-05-31 19:45:52 +0600

    Create TreeNode

--- tree_1.swift
+++ tree_1.swift
@@ -1,12 +1,8 @@
-print("Enter your name and age(seperated by space): - ")
-if let input = readLine() {
-   let params = input.split(separator: " ")
-   if let age = Int(params[1]){
-    let name = String(params[0])
-    print("Name: \(name)\nAge: \(age)")
-   }else{
-    print("Invalid name or age")
-   }
-}else{
-    print("Invalid input")
+final class TreeNode {
+    let value : Int
+    var left : TreeNode?
+    var right : TreeNode?
+    init(value : Int){
+        self.value = value
+    }
 }
