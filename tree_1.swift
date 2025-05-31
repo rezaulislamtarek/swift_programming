3ef3ff605279d5f4732ea88ccb5d6ab61122b912
rezaulislam <mrezaulislam.t@gmail.com>
2025-05-31 19:38:34 +0600

    inout muliple object

--- tree_1.swift
+++ tree_1.swift
@@ -1,6 +1,12 @@
-print("Enter your age:")
-if let input = readLine(), let age = Int(input) {
-    print("your age is \(age) years old.")
-}else {
-    print("Invalid age entered.")
+print("Enter your name and age(seperated by space): - ")
+if let input = readLine() {
+   let params = input.split(separator: " ")
+   if let age = Int(params[1]){
+    let name = String(params[0])
+    print("Name: \(name)\nAge: \(age)")
+   }else{
+    print("Invalid name or age")
+   }
+}else{
+    print("Invalid input")
 }
